5e2d426bc8967b25304e2c606a71f710df21e24f
Giovanni <51537350+vendkura@users.noreply.github.com>
2023-03-12 20:15:33 +0000

    2nd-comit

--- test-pratique-a2sys/src/app/app.module.ts
+++ test-pratique-a2sys/src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AlertModule } from 'ngx-bootstrap/alert';
-
+// To Filter data 
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { AsyncPipe, DecimalPipe,NgFor } from '@angular/common';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+// import {NgbTypeaheadModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,12 +15,13 @@ import { Page1Component } from './page1/page1.component';
 @NgModule({
   declarations: [
     AppComponent,
-    Page1Component
+    Page1Component,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AlertModule.forRoot()
+    AlertModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- test-pratique-a2sys/src/app/page1/page1.component.ts
+++ test-pratique-a2sys/src/app/page1/page1.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, PipeTransform } from '@angular/core';
+import { AsyncPipe, DecimalPipe, NgFor } from '@angular/common';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import operation_data from '../../assets/json/operation_data.json';
+// import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 
+
+interface Operation{
+  id: number| string|undefined,
+  annee: number |string,
+  trimestre: number | string,
+  nature: string | number,
+  reference: string | number,
+  montant: number | string,
+  nom: string | number
+} 
+function search(text:string, pipe:PipeTransform):Operation[]{
+  return operation_data.filter((data)=>{
+    const term = text.toLowerCase();
+    return(
+      data.annee.toString().includes(term) || 
+      pipe.transform(data.nom).includes(term) ||
+			pipe.transform(data.trimestre).includes(term)
+    );
+  } );
+}
 @Component({
   selector: 'app-page1',
   templateUrl: './page1.component.html',
   styleUrls: ['./page1.component.css']
 })
 export class Page1Component {
+  // data$:Observable<Operation[]>;
+  // filter = new FormControl('', { nonNullable: true });
+  operation : Operation[] = operation_data;
 
+//   constructor(pipe: DecimalPipe) {
+// 		this.data$ = this.filter.valueChanges.pipe(
+// 			startWith(''),
+// 			map((text) => search(text, pipe)),
+// 		);
+
+// }
 }
